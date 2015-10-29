50a3f5da101246a89864f4e870060aa75c2d6503
raptor <raptor@bitfighter.org>
2015-10-28 21:41:17 -0600

    Add utility to calculate the resistance to properly bias a BJT base voltage using common resistor values

---
+++ bjt_base_biasing.py
@@ -0,0 +1,131 @@
+'''
+Created on Oct 28, 2015
+
+@author: buckd
+'''
+
+import sys
+
+# Add to these if needed
+COMMON_RESISTOR_MULTIPLES = [10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82]
+
+
+def pick(ratio):
+    best_r1 = None
+    best_r2 = None
+    
+    # Really high to start
+    best = 999999999
+    
+    # Base the magnitude off of the ratio of R1 to R2
+    magnitude = 1;
+    
+    # Lazy...
+    if ratio > 1:
+        magnitude = 1
+    if ratio > 10:
+        magnitude = 10
+    if ratio > 100:
+        magnitude = 100
+    if ratio > 1000:
+        magnitude = 1000
+    
+    # Now loop through all combinations to try and get close to the ratio
+    for r1 in COMMON_RESISTOR_MULTIPLES:
+        r1 = r1 * magnitude
+        
+        for r2 in COMMON_RESISTOR_MULTIPLES:
+            thisRatio = r1 / float(r2)
+            
+            difference = abs(ratio - thisRatio)
+            
+            # Test against previous best match
+            if difference < best:
+                best = difference
+                
+                # Save our better match
+                best_r1 = r1
+                best_r2 = r2
+    
+    print("\n=> Best match:")
+    print("R1:     " + str(best_r1))
+    print("R2:     " + str(best_r2))
+    print("Ratio:  " + str(best_r1/float(best_r2)))
+    
+    
+    # Reset
+    best = 999999999
+    best_r1_1 = None
+    best_r1_2 = None
+    best_r2   = None
+        
+    # Loop through everything again, but add an option to check the addition of
+    # two resistors in series for the numerator
+    for r1_1 in COMMON_RESISTOR_MULTIPLES:
+        r1_1 = r1_1 * magnitude
+        
+        for r1_2 in COMMON_RESISTOR_MULTIPLES:
+            r1_2 = r1_2 * magnitude
+            
+            for r2 in COMMON_RESISTOR_MULTIPLES:
+                thisRatio = (r1_1 + r1_2) / float(r2)
+                
+                difference = abs(ratio - thisRatio)
+                
+                # Test against previous best match
+                if difference < best:
+                    best = difference
+                    
+                    # Save our better match
+                    best_r1_1 = r1_1
+                    best_r1_2 = r1_2
+                    best_r2   = r2
+
+    print "\n=> If you're willing to use 2 resistors for R1:"
+    print("R1:     " + str(best_r1_1) + " + " + str(best_r1_2))
+    print("R2:     " + str(best_r2))
+    print("Ratio:  " + str( (best_r1_1+best_r1_2)/float(best_r2)) )
+    print ""
+    
+    pass
+
+
+def main():
+    # You can hard code these if wanted
+    V_cc = None
+    V_b  = None
+    
+    
+    # Test for parameters
+    if not (V_cc and V_b) and len(sys.argv) < 3:
+        print "BJT Base biasing"
+        print "Usage: " + sys.argv[0] + " [V_cc] [V_b]"
+        print
+        print "Example:"
+        print "  " + sys.argv[0] + " 9 1.08"
+        sys.exit(2)
+    
+    
+    # Grab 
+    if not (V_cc and V_b):
+        V_cc = float(sys.argv[1])
+        V_b = float(sys.argv[2])
+        
+        
+    # Ratio of R1 to R2 is calcualted here
+    ratio = (V_cc - V_b) / V_b
+    
+    print("\n=> Ratio of R1 to R2:  " + str(ratio))
+    
+    pick(ratio)
+    
+    print "\nNOTE: These are multiples.  Scale these if needed."
+    
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
+    pass
+
